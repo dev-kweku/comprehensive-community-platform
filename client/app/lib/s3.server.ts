6a58237a44922a8d21d85e258a1fa8030885a4c3
dev-kweku <frikasoidfrimpong@gmail.com>
2025-08-28 09:53:12 +0000

    server modified

--- client/app/lib/s3.server.ts
+++ client/app/lib/s3.server.ts
@@ -1,39 +1,40 @@
-import { S3Client } from "@aws-sdk/client-s3";
-import { Upload } from "@aws-sdk/lib-storage";
-import aws from "aws-sdk";
-import { Readable } from "node:stream";
+	import { S3Client } from "@aws-sdk/client-s3";
+	import { Upload } from "@aws-sdk/lib-storage";
+	import aws from "aws-sdk";
+	import { Readable } from "node:stream";
 
-const endpoint = new aws.Endpoint(process.env.AWS_UPLOAD_ENDPOINT as string);
+	const endpoint = new aws.Endpoint(process.env.AWS_UPLOAD_ENDPOINT as string);
 
-// @ts-ignore
-const s3 = new S3Client({
+	// @ts-ignore
+	const s3 = new S3Client({
 	endpoint,
 	credentials: {
 		accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
 		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
 	},
 	region: process.env.AWS_REGION as string,
-});
+	forcePathStyle: true, // CRITICAL: Add this to force path-style URLs
+	});
 
-const DIR = process.env.AWS_BUCKET_DIR || 'ttucampus'
+	const DIR = process.env.AWS_BUCKET_DIR || 'ttucampus-dir';
 
-async function upload(
+	async function upload(
 	stream: AsyncIterable<Uint8Array> | Buffer,
 	filename: string,
 	contentType?: string,
-) {
+	) {
 	return new Upload({
 		client: s3,
 		leavePartsOnError: false,
 		params: {
-			ACL: "public-read",
-			Bucket: process.env.AWS_BUCKET as string,
-			Key: [DIR, filename].join('/'),
-			ContentType: contentType,
-			CacheControl: "max-age=31536000",
-			Body: Readable.from(stream),
+		ACL: "public-read",
+		Bucket: process.env.AWS_BUCKET as string,
+		Key: [DIR, filename].join('/'),
+		ContentType: contentType,
+		CacheControl: "max-age=31536000",
+		Body: Readable.from(stream),
 		},
 	}).done();
-}
+	}
 
-export { upload };
+	export { upload };
